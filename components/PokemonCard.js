ec0f35507d4d9ec3beb30ee7ee019817998e7c48
Manish10022001 <manishshirsat2850@gmail.com>
2025-11-29 20:02:33 +0530

    styled card components

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,24 +1,55 @@
 import {ScrollView,View, Text, StyleSheet, Platform, Image} from 'react-native';
 //2:styling card
 //3:card component content
+//4: style the component-> //4.1also create function that return badge styles based on pokemon type0
+                           //4.2 display list of moves and weakness
+//4
+function getTypeDetails(type){
+    switch(type.toLowerCase()){
+        case "electric":
+            return { borderColor: "#FFD700", emoji: "⚡️" };
+        case "water":
+            return { borderColor: "#6493EA", emoji: "💧" };
+        case "fire":
+            return { borderColor: "#FF5733", emoji: "🔥" };
+        case "grass":
+            return { borderColor: "#66CC66", emoji: "🌿" };
+        default:
+            return { borderColor: "#A0A0A0", emoji: "❓" };
+    }
+}
+
 export default function PokemonCard({name,image,type,hp,moves,weaknesses}){
     //3.3 destructure the properties from component prop
+
+    //4.2 invoke function in component
+    const {borderColor, emoji} = getTypeDetails(type);
     return(
         <ScrollView>
         <View style={styles.card}>
-            <View>
-                <Text>{name}</Text>
-                <Text>{hp}</Text>
+            <View style={styles.nameContainer}>
+                <Text style={styles.name}>{name}</Text>
+                <Text style={styles.hp}>💗{hp}</Text>
             </View>
-            <Image source={image} accessibilityLabel={`${name} pokemon`}/>
+            <Image style={styles.image} 
+                    source={image} 
+                    accessibilityLabel={`${name} pokemon`} 
+                    resizeMode="contain"
+            />
 
-            <View>
-                <Text>{type}</Text>
+            <View style={styles.typeContainer}>
+                <View style={[styles.badge, {borderColor}]}>
+                    <Text style={styles.typeEmoji}>{emoji}</Text>
+                    <Text style={styles.typeText}>{type}</Text>
+                </View>
             </View>
 
-            <View>
-                <Text>Moves: {moves.join(', ')}</Text>
-                <Text>Weakness: {weaknesses?.join(', ')}</Text>
+            <View style={styles.movesContainer}>
+                <Text style={styles.movesText}>Moves: {moves.join(', ')}</Text>
+            </View>
+
+            <View style={styles.weaknessContainer}>
+                <Text style={styles.weaknessText}>Weakness: {weaknesses.join(', ')}</Text>
             </View>
         </View>
         </ScrollView>
@@ -43,6 +74,59 @@ const styles = StyleSheet.create({
             android:{
                 elevation:4,
             }
-        })
+        }),
+    },
+    nameContainer:{
+        flexDirection:'row',
+        justifyContent:"space-between",
+        marginBottom:32,
+    },
+    name:{
+        fontSize:32,
+        fontWeight:'bold',
+    },
+    hp:{
+        fontSize:22,
+    },
+    image:{
+        width:"100%",
+        height:200,
+        marginBottom:16,
+    },
+    typeContainer:{
+        flexDirection:"row",
+        justifyContent:"center",
+        alignItems:"center",
+        marginBottom:40,
+    },
+    badge :{
+        flexDirection:"row",
+        alignItems:"center",
+        paddingVertical:6,
+        paddingHorizontal:12,
+        borderRadius:20,
+        borderWidth:4,
+    },
+    typeEmoji:{
+        fontSize:30,
+        marginRight:12,
+    },
+    typeText:{
+        fontSize:22,
+        fontWeight:"bold"
+    },
+    movesContainer:{
+        marginBottom:12,
+    },
+    movesText:{
+        fontSize:18,
+        fontWeight:'bold',
+    },
+    weaknessContainer:{
+        marginBottom:8,
+    },
+    weaknessText:{
+        fontSize:18,
+        fontWeight:'bold'
     }
 })
