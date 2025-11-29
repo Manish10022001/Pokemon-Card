0ebe928222c3fbe568dc856508e9d1b8d72502dd
Manish10022001 <manishshirsat2850@gmail.com>
2025-11-29 12:50:05 +0530

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context'
+import PokemonCard from './components/PokemonCard';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <PokemonCard />
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +14,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ components/PokemonCard.js
@@ -0,0 +1,7 @@
+import {View, Text} from 'react-native';
+
+export default function PokemonCard(){
+    <View>
+        <Text>Pokemon Card</Text>
+    </View>
+}
